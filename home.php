c279ab2d7768cbf6d9daa6faaaac6b6aebab779b
aligojali <ali.au95@gmail.com>
2015-06-14 19:11:51 +0700

    edit baru

--- home.php
+++ home.php
@@ -1,8 +1,7 @@
 <?php session_start();
 include "koneksi.php";
 ?>
-    <!DOCTYPE  html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
+    <html>
 	<head>
 		<title>Ujian Online -- UAS Interpro</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
